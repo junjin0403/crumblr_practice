74bf5175af63b68a063e4217990cb90bdeba6163
Jin Jeon <junjin0403@gmail.com>
2016-08-02 12:02:00 -0400

    Added Support for Image

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801163815) do
+ActiveRecord::Schema.define(version: 20160802151555) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "kind"
   end
 
 end
