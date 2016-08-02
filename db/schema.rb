1d9ee0a3cdeef1ddc96b00f153b379beac203817
Jin Jeon <junjin0403@gmail.com>
2016-08-02 17:54:49 -0400

    Added Hearts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802151555) do
+ActiveRecord::Schema.define(version: 20160802190434) do
+
+  create_table "hearts", force: :cascade do |t|
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
